689d0bea43132572d0b2d5f0cae89927cc73e9a3
Jayden <59065556+Jdare2001@users.noreply.github.com>
2024-05-10 16:52:50 +0100

    assembly differences

--- obj/Debug/net8.0/tree.AssemblyInfo.cs
+++ obj/Debug/net8.0/tree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a63090147adb35170d99439d2ff4059dc1eba0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9b9f4c3310e246d7266d344af27805f3627c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("tree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
